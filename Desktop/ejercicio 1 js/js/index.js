87490af64d2b2a291e309d0f8a621fcce58d1fe1
NicolasCE <nicontreras569@gmail.com>
2020-09-23 21:15:39 -0500

    EJERCICIO JS 1 TERMINADO

--- Desktop/ejercicio 1 js/js/index.js
+++ Desktop/ejercicio 1 js/js/index.js
@@ -1,3 +1,12 @@
 var countries = document.getElementById("countries");
 console.log(countries.value);
 countries.value = "";
+
+console.log(rd1.value);
+rd1.value = "";
+rd1.checked = false;
+if(rd1.checked){
+
+}
+
+
